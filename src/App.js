6f59f422e35c9a61e5b96ab0679df2e404edc27f
apurva999 <apurvasomani111@gmail.com>
2023-04-20 09:53:23 +0530

    components done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Btn } from './components/Button';
+import { Reactb } from './components/Reactbenefits';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Reactb/>
+      <Btn/> 
       </header>
     </div>
   );
